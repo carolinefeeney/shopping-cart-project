9cdad39b29643db275fd99dd684520d7280dffb2
Caroline Feeney <carolinefeeney@Carolines-MacBook-Pro-5.local>
2019-02-02 17:05:37 -0500

    Change receipt to print at end
    Separate input and output display

--- shopping_cart.py
+++ shopping_cart.py
@@ -35,6 +35,7 @@ products = [
 #
 
 total_price = 0
+selected_ids = []
 
 while True:
     selected_id = input("Please input a product identifier: ") #> "9" (string)
@@ -44,21 +45,34 @@ while True:
     if selected_id == "DONE":
         break
     else:
-        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        # need to make sure that datatypes are the same when using == (str)
-        matching_product = matching_products[0]
-        # print(matching_product)
-        # print(type(matching_product))
-        total_price = total_price + matching_product["price"]
-        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"])) #remember to convert numbers to strings when concatenating them
-
-
+        #matching_products = [p for p in products if str(p["id"]) == str(selected_id)] #> need to make sure that datatypes are the same when using == (str)
+        #matching_product = matching_products[0]
+        #total_price = total_price + matching_product["price"]
+        selected_ids.append(selected_id)
 
 #
 # INFO DISPLAY / OUTPUT
 #
 
-print("TOTAL PRICE: " + str(total_price)) #TODO format as USD
+print("---------------------------------")
+print("FEENEY MART")
+print("---------------------------------")
+print("Phone:   908-499-8986")
+print("Web:     www.feeney-mart.com")
+print("Address: 20 W 34th Street ")
+print("         New York, NY 10001")
+print("Checkout Time: ") #TODO add date/time
+print("---------------------------------")
+print("Shopping Cart Items:")
+#print(selected_ids)
+
+for selected_id in selected_ids:
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)] #> need to make sure that datatypes are the same when using == (str)
+    matching_product = matching_products[0]
+    total_price = total_price + matching_product["price"]
+    print("+" + " " + "SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"])) #> remember to convert numbers to strings when concatenating them
+
+print("Subtotal: " + str(total_price)) #TODO format as USD
 
 
 # grocery store name of your choice.
